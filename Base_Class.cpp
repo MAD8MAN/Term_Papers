637cff0abab27005afd487a9a0a0714d7ca0723d
MADMAN <97067649+MAD8MAN@users.noreply.github.com>
2023-05-22 20:23:13 +0300

    Create Base_Class.cpp

---
+++ Base_Class.cpp
@@ -0,0 +1,230 @@
+#include "Base_Class.h"
+using namespace std;
+second sec;
+
+void PC_1::Working()
+{
+	int x;
+	char z;
+	cout << "1.Узнать Информацию о устройстве\n";
+	cout << "2.Изменить Название ПК\n";
+	cout << "3.Вернутся\n";
+	cin >> x;
+	switch (x)
+	{
+	case 1:
+	{
+			  cout << Name_PC << endl;
+			  cout << IP;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+			  break;
+			  if (z != 'y')
+				  sec.sf();
+
+	}
+	case 2:
+	{
+			  string NewName;
+			  cout << "Введите новое название: ";
+			  cin >> NewName;
+			  Name_PC = NewName;
+			  cout << "Присвоено новое имя " << Name_PC;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+
+
+
+	}
+	case 3:
+	{
+			  system("cls");
+			  sec.sf();
+	}
+	}
+}
+
+void PC_2::Working()
+{
+	int x;
+	char z;
+	cout << "1.Узнать Информацию о устройстве\n";
+	cout << "2.Изменить Название ПК\n";
+	cout << "3.Вернутся\n";
+	cin >> x;
+	switch (x)
+	{
+	case 1:
+	{
+			  cout << Name_PC << endl;
+			  cout << IP;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+			  break;
+			  if (z != 'y')
+				  sec.sf();
+
+	}
+	case 2:
+	{
+			  string NewName;
+			  cout << "Введите новое название: ";
+			  cin >> NewName;
+			  Name_PC = NewName;
+			  cout << "Присвоено новое имя " << Name_PC;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+
+
+
+	}
+	case 3:
+	{
+			  system("cls");
+			  sec.sf();
+	}
+	}
+}
+
+void PC_3::Working()
+{
+	int x;
+	char z;
+	cout << "1.Узнать Информацию о устройстве\n";
+	cout << "2.Изменить Название ПК\n";
+	cout << "3.Вернутся\n";
+	cin >> x;
+	switch (x)
+	{
+	case 1:
+	{
+			  cout << Name_PC << endl;
+			  cout << IP;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+			  break;
+			  if (z != 'y')
+				  sec.sf();
+
+	}
+	case 2:
+	{
+			  string NewName;
+			  cout << "Введите новое название: ";
+			  cin >> NewName;
+			  Name_PC = NewName;
+			  cout << "Присвоено новое имя " << Name_PC;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+
+
+
+	}
+	case 3:
+	{
+			  system("cls");
+			  sec.sf();
+	}
+	}
+}
+
+void PC_4::Working()
+{
+	int x;
+	char z;
+	cout << "1.Узнать Информацию о устройстве\n";
+	cout << "2.Изменить Название ПК\n";
+	cout << "3.Вернутся\n";
+	cin >> x;
+	switch (x)
+	{
+	case 1:
+	{
+			  cout << Name_PC << endl;
+			  cout << IP;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+			  break;
+			  if (z != 'y')
+				  sec.sf();
+
+	}
+	case 2:
+	{
+			  string NewName;
+			  cout << "Введите новое название: ";
+			  cin >> NewName;
+			  Name_PC = NewName;
+			  cout << "Присвоено новое имя " << Name_PC;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+
+
+
+	}
+	case 3:
+	{
+			  system("cls");
+			  sec.sf();
+	}
+	}
+}
+
+void PC_5::Working()
+{
+	int x;
+	char z;
+	cout << "1.Узнать Информацию о устройстве\n";
+	cout << "2.Изменить Название ПК\n";
+	cout << "3.Вернутся\n";
+	cin >> x;
+	switch (x)
+	{
+	case 1:
+	{
+			  cout << Name_PC << endl;
+			  cout << IP;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+			  break;
+			  if (z != 'y')
+				  sec.sf();
+
+	}
+	case 2:
+	{
+			  string NewName;
+			  cout << "Введите новое название: ";
+			  cin >> NewName;
+			  Name_PC = NewName;
+			  cout << "Присвоено новое имя " << Name_PC;
+			  cout << "\nПродолжить пользоваться устройством?(y/n) ";
+			  cin >> z;
+			  if (z == 'y')
+				  Working();
+	}
+	case 3:
+	{
+			  system("cls");
+			  sec.sf();
+	}
+	}
+}
