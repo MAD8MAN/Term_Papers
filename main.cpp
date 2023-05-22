f047f3e81b88b5e295d152ec5521b87db53cb932
MADMAN <97067649+MAD8MAN@users.noreply.github.com>
2023-05-22 20:22:27 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "Base_Class.h"
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "\tДля обновления списка доступных ПК Нажмине Enter";
+	cin.ignore();
+	second sec;
+	sec.sf();
+	system("pause>NULL");
+}
